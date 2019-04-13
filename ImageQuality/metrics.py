cd7bb462d9fdbafdf896870ad8278c8c731ae266
Simon <fake@example.com>
2019-04-13 13:02:13 +0200

    penis

--- ImageQuality/metrics.py
+++ ImageQuality/metrics.py
@@ -1,20 +1,10 @@
-import numpy as np
-import math
 import cv2
 import sys
+import os
 from sewar.full_ref import *
 # sewar-lib from https://github.com/andrewekhalel/sewar
 
-if len(sys.argv) < 4:
-	print("correct usage:      python metrics.py -[metric] sourceFile1 compareFile2\n\n"
-        	"supported formats:  png, jpg, jp2 (not jxr, bpg)\n"
-        	"supported metrics:  mse, rmse, psnr, rmse_sw, uqi, ssim, ergas, \n"
-        	"\t\t    scc, rase, sam, msssim, vifp");
-else:
-	metric = sys.argv[1][1:]
-	src = cv2.imread(sys.argv[2])
-	comp = cv2.imread(sys.argv[3],1)
-	
+def getMetric(metric):
 	if metric == "mse":
 		score = mse(src, comp)
 	elif metric == "rmse":
@@ -41,5 +31,30 @@ else:
 		score = vifp(src, comp)
 	else:
 		print("Metric ", metric, " is NOT supported")
+	return score
+
+def getFilename(path):
+	return os.path.basename(path)[:-4]
+
+
+if len(sys.argv) < 4:
+	print("correct usage:      python metrics.py -[metric] referenceFile1 compareFile2\n\n"
+        	"supported formats:  png, jpg, jp2 (not jxr, bpg)\n"
+        	"supported metrics:  mse, rmse, psnr, rmse_sw, uqi, ssim, ergas, \n"
+        	"\t\t    scc, rase, sam, msssim, vifp");
+else:
+	f = open('scores.txt', 'w')
+	metric = sys.argv[1][1:]
+	src = cv2.imread(sys.argv[2])  # reference image
+	sys.argv[2] = getFilename(sys.argv[2])
+
+	for x in range(3, len(sys.argv)):
+		comp = cv2.imread(sys.argv[x],1)  # image to compare
+		sys.argv[x] = getFilename(sys.argv[x])
+		tmp_score = getMetric(metric)
+		out = "\n{} -> {}\n{}:\t{}".format(sys.argv[2], sys.argv[x], metric.upper(),tmp_score)
+		print(out)
+		f.write(out)
+
 	
-	print(metric.upper(), ":\t", score)
+
